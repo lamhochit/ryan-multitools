4be7a4f70ff70762c9b1dd0516650dd8daf76618
Ryan <lamhochit@icloud.com>
2022-12-20 22:50:24 +0800

    ADDEd username random gen function

--- telegram-master/tg-master.py
+++ telegram-master/tg-master.py
@@ -1,12 +1,17 @@
 import asyncio
 import datetime as dt
+import random
+import string
 from telethon import TelegramClient, events
 from telethon.sessions import MemorySession
+from telethon.tl.functions.account import UpdateUsernameRequest
+from telethon.errors.rpcerrorlist import UsernameNotModifiedError, UsernameInvalidError, UsernameOccupiedError
 
 
 # credentials
 api_id = 17377816
 api_hash = '1d7c81bf09b72bf151a4cc67471fc64f'
+phone = '+88805413186'
 
 
 client = TelegramClient(MemorySession(), api_id, api_hash)
@@ -28,6 +33,17 @@ async def self_delete_handler(event):
     if sender.username == me.username:
         await asyncio.sleep(600)
         await event.delete()
+
+
+# Auto changes my username whenever there's a message
+@client.on(events.NewMessage())
+async def change_username_handler(event):
+    try:
+        characters = string.ascii_letters + string.digits
+        username = ''.join(random.choice(characters) for i in range(10))
+        await client(UpdateUsernameRequest(username=username))
+    except (UsernameNotModifiedError, UsernameInvalidError, UsernameOccupiedError) as e:
+        pass
 
 
 if __name__ == '__main__':
